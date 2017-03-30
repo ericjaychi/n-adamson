a7d9047bbce7a038568b5f13f0ed9f8d58048046
Eric Chi <ericjaychi@gmail.com>
2017-03-30 11:17:43 -0700

    Add XSS checks

--- web/content/contact.php
+++ web/content/contact.php
@@ -69,22 +69,24 @@
 					<div class="col-md-6">
 						<?php
 							if(isset($_POST['submit'])){
-								$to = "ericjaychi@gmail.com"; // this is your Email address
-								$from = $_POST['email']; // this is the sender's Email address
-								$name = $_POST['name'];
-								$subject = "Form submission";
-								$subject2 = "Copy of your form submission";
-								$message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-								$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+								/* TODO: Change this to client's email. */
+								$to = "ericjaychi@gmail.com";
+
+								$from = stripcslashes($_POST['email']);
+								$name = stripcslashes($_POST['name']);
+								$body = stripcslashes($_POST['message']);
+
+								$subject = "Someone Contacted You!";
+								$subject2 = "Copy of your message";
+								$message = $name . " wrote the following:" . "\n\n" . $body;
+								$message2 = "Here is a copy of your message " . $name . "\n\n" . $body;
 
 								$headers = "From:" . $from;
 								$headers2 = "From:" . $to;
 								mail($to, $subject, $message, $headers);
-								mail($from, $subject2, $message2, $headers2); // sends a copy of the message to the sender
-
+								mail($from, $subject2, $message2, $headers2);
 
 								echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-								// You can also use header('Location: thank_you.php'); to redirect to another page.
 							}
 						?>
 						<form action="" method="post">
