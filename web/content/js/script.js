9296d6065abc530d68c6c3528336888a5d17b058
Eric Chi <ericjaychi@gmail.com>
2017-03-30 22:37:56 -0700

    Fix opacity overlay bug with filter

--- web/content/js/script.js
+++ web/content/js/script.js
@@ -1,13 +1,16 @@
 $(function() {
-    var selectedClass = "";
-    $(".fil-cat").click(function() {
-        selectedClass = $(this).attr("data-rel");
-        $("#portfolio").fadeTo(100, 0.1);
-        $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
-        setTimeout(function() {
-            $("." + selectedClass).fadeIn().addClass('scale-anm');
-            $("#portfolio").fadeTo(300, 1);
-        }, 300);
+	$(".fil-cat").click(function() {
+		var selectedClass = "." + $(this).attr("data-rel");
 
-    });
+		$("#portfolio").fadeTo(100, 0.1);
+
+		$("#portfolio div").not(selectedClass).children("div", "span").fadeOut().removeClass('scale-anm');
+		$("#portfolio div").not(selectedClass).fadeOut().removeClass('scale-anm');
+
+		setTimeout(function() {
+			$(selectedClass).children("div", "span").fadeIn().addClass('scale-anm');
+			$(selectedClass).fadeIn().addClass('scale-anm');
+			$("#portfolio").fadeTo(300, 1);
+		}, 300);
+	});
 });
