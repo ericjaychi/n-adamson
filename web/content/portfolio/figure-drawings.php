e0f0bb5911ce095eba912171bca9e23b525989fa
Eric Chi <ericjaychi@gmail.com>
2017-07-19 22:53:32 -0700

    Remove unwanted images

--- web/content/portfolio/figure-drawings.php
+++ web/content/portfolio/figure-drawings.php
@@ -57,12 +57,7 @@
 						</a>
 						<a href="http://res.cloudinary.com/dzf64w6kx/image/upload/v1500529703/n-adamson/personal/sketches-characters.jpg" data-lightbox="roadtrip">
 							<div class="tile scale-anm sketches all">
-								<img class="nested-page-image" src="http://res.cloudinary.com/dzf64w6kx/image/upload/v1500529703/n-adamson/personal/sketches-characters.jpg" alt="" />
-							</div>
-						</a>
-						<a href="http://res.cloudinary.com/dzf64w6kx/image/upload/v1498153527/n-adamson/personal/sketches-final-compile-test-figures-ample-newer-enviro-sketches-pg2_mini.jpg" data-lightbox="roadtrip">
-							<div class="tile scale-anm sketches all">
-								<img src="http://res.cloudinary.com/dzf64w6kx/image/upload/v1498153527/n-adamson/personal/sketches-final-compile-test-figures-ample-newer-enviro-sketches-pg2_mini.jpg" alt="" />
+								<img src="http://res.cloudinary.com/dzf64w6kx/image/upload/v1500529703/n-adamson/personal/sketches-characters.jpg" alt="" />
 							</div>
 						</a>
 					</div>
