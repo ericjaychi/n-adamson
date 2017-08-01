bf3934aec05621c5d5c0eb744c00f630b52f3c7f
Eric Chi <ericjaychi@gmail.com>
2017-07-31 21:18:04 -0700

    Add portfolio to footer

--- web/content/portfolio/project-water-ii.php
+++ web/content/portfolio/project-water-ii.php
@@ -81,7 +81,7 @@
 				<div class="row">
 					<div class="col-md-12">
 						<p class="resume">
-							<a href="../pdfs/resume.pdf">RESUME</a> | <a href="https://www.linkedin.com/in/nicole-adamson-a1730988/">LINKEDIN</a>
+							<a href="../pdfs/resume.pdf">RESUME</a> | <a href="../pdfs/portfolio.pdf">PORTFOLIO</a> | <a href="https://www.linkedin.com/in/nicole-adamson-a1730988/">LINKEDIN</a>
 						</p>
 					</div>
 				</div>
