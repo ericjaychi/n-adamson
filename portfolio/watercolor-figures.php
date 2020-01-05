556aedcbd16b345143c997faa7440fb7ca6f4134
Eric Chi <ericjaychi@gmail.com>
2020-01-05 12:43:46 -0800

    Remove extra footer on watercolor-figures.php

--- portfolio/watercolor-figures.php
+++ portfolio/watercolor-figures.php
@@ -79,20 +79,6 @@
 					</div>
 				</div><hr>
 			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<p class="resume">
-							<a href="../pdfs/resume.pdf">RESUME</a> | <a href="../pdfs/portfolio.pdf">PORTFOLIO</a> | <a href="https://www.linkedin.com/in/nicole-adamson-a1730988/">LINKEDIN</a>
-						</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<p class="copyright">© 2019 NICOLE ADAMSON. ANY DISTRIBUTION OR ALTERATIONS OF ANY IMAGE IS PROHIBITED WITHOUT PERMISSION.</p>
-					</div>
-				</div>
-			</div>
 			<?php include "../includes/footer.php" ?>
 		</div>
 	</body>
